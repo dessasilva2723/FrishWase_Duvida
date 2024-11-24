81267b7b47d6ea4df24333746d69b2f75f41c90b
dessasilva2723 <00001103668985sp@al.educacao.sp.gov.br>
2024-11-24 18:31:25 -0300

    Add files via upload

---
+++ FrishWase_Dúvidas/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Dúvidas',
+    'Quais são os recursos de segurança do produto?',
+    'O frigobar possui recursos como travas digitais e senhas para evitar acessos não autorizados. Além disso, ele envia alertas se a porta ficar aberta por muito tempo ou se houver problemas com a temperatura.'
+)
+
+criaCartao(
+    'Dúvidas',
+    'Qual é a capcidade do frigobar?',
+    'O frigobar está disponível em diferentes capacidades para atender às suas necessidades, desde modelos compactos para pequenos espaços até versões maiores que podem acomodar mais alimentos e bebidas.'
+)
+
+criaCartao(
+    'Dúvidas',
+    'Qual é o consumo de energia do frigobar inteligente?',
+    'O frigobar inteligente da Frishwase é projetado para ser eficiente em termos de consumo de energia, utilizando tecnologia que minimiza o gasto elétrico enquanto mantém seus alimentos frescos.'
+)
+
+criaCartao(
+    'Dúvidas',
+    'Como funciona o frigobar inteligente?',
+    'O frigobar inteligente da Frishwase utiliza tecnologia de conectividade, como Wi-Fi ou Bluetooth, para se conectar ao seu smartphone ou outros dispositivos. Ele pode monitorar a temperatura interna, controlar a iluminação e até enviar notificações quando a porta está aberta ou quando a temperatura sai do intervalo ideal.'
+)
